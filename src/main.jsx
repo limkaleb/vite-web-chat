cca35039ad0ffb957f3a17770d18d0f5fa52506e
Kaleb Lim <kaleb@pickupp.io>
2023-11-24 16:44:42 +0700

    feat(implement-code): apply persist wrapper to root

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+import stores from './stores'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={stores}>
+      <PersistGate loading={null} persistor={persistStore(stores)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
